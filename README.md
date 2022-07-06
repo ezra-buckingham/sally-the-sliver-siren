18d90f1c1c84ccd5082f9b0dd669de4f36bef6ad
Ezra Buckingham <ezra.buckingham@gmail.com>
2022-07-06 09:28:13 -0400

    Added config file ability

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sally the Slier Siren
 
-A custom sliver client that will send slack notifications upon new beacons / sessions that check in.
+A custom sliver client that will send slack notifications upon new beacons / sessions that check in. Can either be run with a config file (that will be reloaded after `sleep` number of seconds, except for the `sliver_config` as the intial connection is used for all checks).
 
 ## Usage
 
@@ -13,19 +13,39 @@ pip3 install -r requirements.txt
 Once all dependencies are installed, you can then run the python script (which will run continuously).
 
 ```
-usage: sally.py [-h] [-c SLIVER_CONFIG] [-u SLACK_URL] [-s SLEEP]
+usage: sally.py [-h] [-c CONFIG] [-S SLIVER_CONFIG] [-u SLACK_URL] [-s SLEEP]
 
 Custom Sliver client that will will emit events and send webhook notifications when new beacons / sessions check in.
 
 optional arguments:
   -h, --help            show this help message and exit
-  -c SLIVER_CONFIG, --sliver_config SLIVER_CONFIG
+  -c CONFIG, --config CONFIG
+                        Path to sally config file (which will immediately take effect as changes to Sally)
+  -S SLIVER_CONFIG, --sliver_config SLIVER_CONFIG
                         Path to the sliver config to connect with
   -u SLACK_URL, --slack_url SLACK_URL
                         Slack URL to send notifications to
   -s SLEEP, --sleep SLEEP
                         Sleep time in between checking for changes in beacons / sessions
 ```
+
+### Usage with Command Line
+
+If you want to run the command using only the command line arguments, a sample command would look like the following:
+
+```bash
+python3 ./sally.py -S /path/to/sliver-config.conf -s 10 -u https://slack.com
+```
+
+### Usage with Config File
+
+If you choose to run the custom client as a service on the host, the most convenient way to manage the arguments is via a configuration file that will propogate changes into the running service every `sleep` number of seconds.
+
+```bash
+python3 ./sally.py -c /path/to/sally-config.conf
+```
+
+For convenience, there is a `config_example.yml` file included in the repo (default search location for Sally is `./config.yml`).
 
 ## Running Continuously
 
--- sally.py
+++ sally.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
 
 import asyncio
+from nis import cat
 from pathlib import Path
 from time import sleep
 import argparse
 import requests
 from sliver import *
+import yaml
 
 async def main():
     parser = argparse.ArgumentParser(description='Custom Sliver client that will will emit events and send webhook notifications when new beacons / sessions check in.')
-    parser.add_argument('-c', '--sliver_config', type=Path, help='Path to the sliver config to connect with')
-    parser.add_argument('-u', '--slack_url', type=str, help='Slack URL to send notifications to' )
-    parser.add_argument('-s', '--sleep', type=int, default=1, help='Sleep time in between checking for changes in beacons / sessions')
+    parser.add_argument('-c', '--config', type=Path, help='Path to sally config file (which will immediately take effect as changes to Sally)')
+    parser.add_argument('-S', '--sliver_config', default=None, type=Path, help='Path to the sliver config to connect with')
+    parser.add_argument('-u', '--slack_url', default=None, type=str, help='Slack URL to send notifications to' )
+    parser.add_argument('-s', '--sleep', type=int, default=10, help='Sleep time in between checking for changes in beacons / sessions')
 
-    
     args = parser.parse_args()
+    sally_config = args.config
     sliver_config = args.sliver_config
     slack_url = args.slack_url
     sleep_time = args.sleep
 
+    # Flag to represent if we are using a config file
+    using_config = False
+
+    if sally_config and not sliver_config and not sliver_config:
+        using_config = True
+        sliver_config, slack_url, sleep_time = read_config(sally_config)
+
     if not sliver_config.exists():
         print('[X] Sliver config not found at location provided')
         exit(1)
-    
 
     config = SliverClientConfig.parse_config_file(sliver_config)
     client = SliverClient(config)
@@ -34,6 +43,7 @@ async def main():
     original_beacons = await client.beacons()
     print('[*] Getting current Sliver sessions')
     original_sessions = await client.sessions()
+
 
     while True:
         current_beacons = await client.beacons()
@@ -55,9 +65,13 @@ async def main():
                 post_data = generate_slack_message(new_beacon_data, 'beacon')
 
                 print(f'[*] Sending message about beacon {new_beacon}')
-                resp = requests.post(slack_url, json=post_data)
-                print(f'[*] Sent message: HTTP ({resp.status_code})')
-        
+                try:
+                    resp = requests.post(slack_url, json=post_data)
+                    print(f'[*] Sent message: HTTP ({resp.status_code})')
+                except:
+                    print(f'[X] Something went wrong when sending webhook message, check the URL used')
+                
+            
         else:
             print('[*] No new beacons found')
 
@@ -72,14 +86,29 @@ async def main():
                 post_data = generate_slack_message(new_session_data, 'session')
 
                 print(f'[*] Sending message about session {new_session}')
-                resp = requests.post(slack_url, json=post_data)
-                print(f'[*] Sent message: HTTP ({resp.status_code})')
-        
+                try:
+                    resp = requests.post(slack_url, json=post_data)
+                    print(f'[*] Sent message: HTTP ({resp.status_code})')
+                except:
+                    print(f'[X] Something went wrong when sending webhook message, check the URL used')
+            
         else:
             print('[*] No new sessions found')
 
         print(f'[*] Sleeping for { sleep_time } seconds')
         sleep(sleep_time)
+
+        # Get new config values
+        if using_config:
+            sliver_config, slack_url, sleep_time = read_config(sally_config)
+
+   
+
+def read_config(config_path):
+    config_file = Path(config_path).read_text()
+    config_file_yaml = yaml.safe_load(config_file)
+
+    return config_file_yaml['sliver_config'], config_file_yaml['slack_url'], config_file_yaml['sleep_time']
 
 
 def extract_uuid(beacon_list):
