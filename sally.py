f228b115027e5423a0af6c205cbe1e1373bba8f0
galoryber <51980089+galoryber@users.noreply.github.com>
2024-02-15 14:27:26 -0600

    Update sally.py
    Update to parse config, or accept params. New function to allow teams notifications via incoming webhook, in addition to slack notifications

--- sally.py
+++ sally.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
-import asyncio
-from nis import cat
+import asyncio, os, argparse, requests, yaml
 from pathlib import Path
 from time import sleep
-import argparse
-import requests
 from sliver import *
-import yaml
 
 async def main():
     parser = argparse.ArgumentParser(description='Custom Sliver client that will will emit events and send webhook notifications when new beacons / sessions check in.')
     parser.add_argument('-c', '--config', type=Path, help='Path to sally config file (which will immediately take effect as changes to Sally)')
     parser.add_argument('-S', '--sliver_config', default=None, type=Path, help='Path to the sliver config to connect with')
-    parser.add_argument('-u', '--slack_url', default=None, type=str, help='Slack URL to send notifications to' )
+    parser.add_argument('-u', '--url', default=None, type=str, help='Webhook URL to send notifications to' )
+    parser.add_argument('-p', '--provider', default=None, type=str, help='Slack or Teams' )
     parser.add_argument('-s', '--sleep', type=int, default=10, help='Sleep time in between checking for changes in beacons / sessions')
 
     args = parser.parse_args()
     sally_config = args.config
     sliver_config = args.sliver_config
-    slack_url = args.slack_url
+    provider = args.provider
+    webhook_url = args.url
     sleep_time = args.sleep
 
     # Flag to represent if we are using a config file
@@ -27,14 +25,15 @@ async def main():
 
     if sally_config and not sliver_config and not sliver_config:
         using_config = True
-        sliver_config, slack_url, sleep_time = read_config(sally_config)
+        sliver_config, provider, webhook_url, sleep_time = read_config(sally_config)
 
-    if not sliver_config.exists():
-        print('[X] Sliver config not found at location provided')
+    if not os.path.exists(sliver_config):
+        print('[X] Sliver config not found at ' + str(sliver_config))
         exit(1)
 
     config = SliverClientConfig.parse_config_file(sliver_config)
     client = SliverClient(config)
+
     print('[*] Connecting to Sliver Server...')
     await client.connect()
     print('[*] Connected to Sliver Server!')
@@ -62,12 +61,15 @@ async def main():
                 new_beacon_data = [
                     selected_beacon for selected_beacon in current_beacons if selected_beacon.ID == new_beacon]
                 new_beacon_data = new_beacon_data[0]
-                post_data = generate_slack_message(new_beacon_data, 'beacon')
-
+                if str(provider).lower() == 'teams':
+                    post_data = generate_teams_message(new_beacon_data, 'beacon')
+                elif str(provider).lower() == 'slack':
+                    post_data = generate_slack_message(new_beacon_data, 'beacon')
                 print(f'[*] Sending message about beacon {new_beacon}')
                 try:
-                    resp = requests.post(slack_url, json=post_data)
+                    resp = requests.post(webhook_url, json=post_data)
                     print(f'[*] Sent message: HTTP ({resp.status_code})')
+
                 except:
                     print(f'[X] Something went wrong when sending webhook message, check the URL used')
                 
@@ -83,12 +85,16 @@ async def main():
                 new_session_data = [
                     selected_session for selected_session in current_sessions if selected_session.ID == new_session]
                 new_session_data = new_session_data[0]
-                post_data = generate_slack_message(new_session_data, 'session')
+                if str(provider).lower() == 'teams':
+                    post_data = generate_teams_message(new_session_data, 'session')
+                elif str(provider).lower() == 'slack':
+                    post_data = generate_slack_message(new_session_data, 'session')
 
                 print(f'[*] Sending message about session {new_session}')
                 try:
-                    resp = requests.post(slack_url, json=post_data)
+                    resp = requests.post(webhook_url, json=post_data)
                     print(f'[*] Sent message: HTTP ({resp.status_code})')
+
                 except:
                     print(f'[X] Something went wrong when sending webhook message, check the URL used')
             
@@ -100,7 +106,7 @@ async def main():
 
         # Get new config values
         if using_config:
-            sliver_config, slack_url, sleep_time = read_config(sally_config)
+            sliver_config, provider, webhook_url, sleep_time = read_config(sally_config)
 
    
 
@@ -108,7 +114,7 @@ def read_config(config_path):
     config_file = Path(config_path).read_text()
     config_file_yaml = yaml.safe_load(config_file)
 
-    return config_file_yaml['sliver_config'], config_file_yaml['slack_url'], config_file_yaml['sleep_time']
+    return config_file_yaml['sliver_config'], config_file_yaml['provider'], config_file_yaml['url'], config_file_yaml['sleep_time']
 
 
 def extract_uuid(beacon_list):
@@ -116,6 +122,43 @@ def extract_uuid(beacon_list):
 
     return set(beacon_uuids)
 
+def generate_teams_message(data, type='beacon'):
+    
+    header_text = '🚨  New Sliver '
+    
+    if type == 'beacon':
+        header_text += 'Beacon'
+    elif type == 'session':
+        header_text += 'Session'
+    else:
+        header_text += 'Event'
+
+    post_data = {
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    "themeColor": "0076D7",
+    "summary": header_text,
+    "sections": [{
+        "activityTitle": header_text,
+        "activitySubtitle": "C2 Domain: " + data.ActiveC2,
+        "activityImage": "https://images.bishopfox.com/prod-1437/Images/global/main-menu/poly-fox-graphic.png",
+        "facts": [{
+            "name": "Hostname",
+            "value": data.Hostname
+        }, {
+            "name": "Username",
+            "value": data.Username
+        }, {
+            "name": "Process ID",
+            "value": data.PID
+        }, {
+            "name": "OS",
+            "value": data.OS
+        }],
+        "markdown": "true"
+    }]
+    }
+    return post_data
 
 def generate_slack_message(data, type='beacon'):
     header_text = '🚨  New Sliver '
